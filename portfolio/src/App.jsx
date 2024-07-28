9e06467c2ff9c3f9e47ffb8a1cfee5fbd1b055e6
sapna maurya <io5.sapnamaurya@gmail.com>
2024-07-28 11:59:56 +0530

    port

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./component/Header";
 import Home from "./component/Home";
+import About from "./component/About";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
       <Header />
       <Home />
+      <About />
     </div>
   );
 }
