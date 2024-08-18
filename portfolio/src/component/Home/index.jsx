feff81780f087646d13fb5070b928bb631a031ec
sapna maurya <138869250+sapnamaurya@users.noreply.github.com>
2024-08-18 14:00:27 +0530

    potfolio

--- portfolio/src/component/Home/index.jsx
+++ portfolio/src/component/Home/index.jsx
@@ -1,30 +1,7 @@
 import React from "react";
-import "./style.css";
-import img from "../../assest/images/sapu img.png";
-const Home = () => {
-  return (
-    <>
-      <div className=" conatiner home-sec">
-        <div className="left">
-          <h3>Hi,</h3>
-          <h1 className="name">
-            I'm <span>Sapna Maurya</span>
-          </h1>
-          <h1 className="title">Frontend Developer</h1>
-          <p>
-            I'm a Frontend Developer with extensive experience for over 1
-            year,expertise is to create and website design,Frontend Design and
-            many more...
-          </p>
 
-          <button className="home-btn">Experience</button>
-        </div>
-        <div className="right">
-          <img src={img} />
-        </div>
-      </div>
-    </>
-  );
+const Home = () => {
+  return <div>home</div>;
 };
 
 export default Home;
