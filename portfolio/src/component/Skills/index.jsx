60236c742e4b2484e749c8060b5d1d0959961ba5
sapna maurya <138869250+sapnamaurya@users.noreply.github.com>
2024-09-04 14:35:20 +0530

    port

--- portfolio/src/component/Skills/index.jsx
+++ portfolio/src/component/Skills/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { skills } from "../../data/data.js";
+import { skills } from "../../data/data.jsx";
 
 const Container = styled.div`
   display: flex;
